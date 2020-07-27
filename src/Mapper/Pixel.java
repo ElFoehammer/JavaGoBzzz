35b5f627e5de12924acb29464b6f430f861fd5d9
Ale_Lizarraga <Alejandro@17SAM8A13.local>
2020-07-26 23:51:07 -0500

    First commit

---
+++ src/Mapper/Pixel.java
@@ -0,0 +1,18 @@
+package Mapper;
+
+public class Pixel {
+	
+	private int r;
+	private int g;
+	private int b;
+	private int x;
+	private int y;
+	
+	public Pixel(int r, int g, int b, int x, int y) {
+		this.r = r;
+		this.g = g;
+		this.b = b;
+		this.x = x;
+		this.y = y;
+	}
+}
